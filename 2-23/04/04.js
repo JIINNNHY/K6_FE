db843fa5de4ac9c96fa6e60896271743f55ac831
JIINNNHY <jiinnnhy@outlook.com>
2024-02-23 17:49:55 +0900

    d23m2v2

--- 2-23/04/04.js
+++ 2-23/04/04.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const bts = document.querySelectorAll(".bt");
-    const btnImg = document.querySelector("#vImg");
+    const btnImg = document.querySelectorAll(".vImg");
     
     const img = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
     
-    bts.forEach((bt) => {
+    bts.forEach((bt, index) => {
         bt.addEventListener("click", () => {
-            const cImg = img[Math.floor(Math.random() * img.length)];
-            btnImg.src = `./img/${cImg}`;
+                const cImg = img[Math.floor(Math.random() * img.length)];
+                btnImg[index].src = `./img/${cImg}`;
         });
     });
 });
